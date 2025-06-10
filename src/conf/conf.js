213038b3d8257db20a25d27b677947e74733c6e4
GTAJIT <mukhujeegtajit@gmail.com>
2025-06-10 22:12:42 +0530

    idk

---
+++ src/conf/conf.js
@@ -0,0 +1,10 @@
+const conf = {
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDBId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+    TineyMCEAPI: String(import.meta.env.VITE_TINEYMCE_API)
+}
+
+export default conf
